0357b2aa50c3881a4bdc2672b9a35b43274ddc4a
staneee <yi.hang@live.com>
2020-03-15 15:58:10 +0800

    添加Riven模块

---
+++ src/Riven/Check.cs
@@ -0,0 +1,58 @@
+using JetBrains.Annotations;
+using Riven.Collections.Extensions;
+using Riven.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Riven
+{
+    [DebuggerStepThrough]
+    public static class Check
+    {
+        [ContractAnnotation("value:null => halt")]
+        public static T NotNull<T>(T value, [InvokerParameterName] [NotNull] string parameterName)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(parameterName);
+            }
+
+            return value;
+        }
+
+        [ContractAnnotation("value:null => halt")]
+        public static string NotNullOrEmpty(string value, [InvokerParameterName] [NotNull] string parameterName)
+        {
+            if (value.IsNullOrEmpty())
+            {
+                throw new ArgumentException($"{parameterName} can not be null or empty!", parameterName);
+            }
+
+            return value;
+        }
+
+        [ContractAnnotation("value:null => halt")]
+        public static string NotNullOrWhiteSpace(string value, [InvokerParameterName] [NotNull] string parameterName)
+        {
+            if (value.IsNullOrWhiteSpace())
+            {
+                throw new ArgumentException($"{parameterName} can not be null, empty or white space!", parameterName);
+            }
+
+            return value;
+        }
+
+        [ContractAnnotation("value:null => halt")]
+        public static ICollection<T> NotNullOrEmpty<T>(ICollection<T> value, [InvokerParameterName] [NotNull] string parameterName)
+        {
+            if (value.IsNullOrEmpty())
+            {
+                throw new ArgumentException(parameterName + " can not be null or empty!", parameterName);
+            }
+
+            return value;
+        }
+    }
+}
