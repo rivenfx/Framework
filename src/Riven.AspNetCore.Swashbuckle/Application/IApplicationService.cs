456b97dae7a1c7a8b0285aa4533c12d54fb5a73a
staneee <yi.hang@live.com>
2020-03-16 16:22:14 +0800

    增加IApplicationService

---
+++ src/Riven.AspNetCore.Swashbuckle/Application/IApplicationService.cs
@@ -0,0 +1,13 @@
+using Panda.DynamicWebApi;
+using Panda.DynamicWebApi.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riven.Application
+{
+    [DynamicWebApi]
+    public interface IApplicationService : IDynamicWebApi
+    {
+    }
+}
