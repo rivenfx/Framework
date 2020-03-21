0ffa9e0e03633bddc09a97427cd9320e4400111c
staneee <yi.hang@live.com>
2020-03-21 17:46:18 +0800

    增加依赖注入的扩展函数

---
+++ src/Riven/Dependency/RivenDependencyExtensions.cs
@@ -0,0 +1,72 @@
+using Microsoft.Extensions.DependencyInjection;
+using Riven.Dependency;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Riven
+{
+    /// <summary>
+    /// Riven 依赖注入扩展
+    /// </summary>
+    public static class RivenDependencyExtensions
+    {
+        /// <summary>
+        /// 根据类型自动依赖注入
+        /// </summary>
+        /// <typeparam name="TType"></typeparam>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection RegisterAssemblyOf<TType>(this IServiceCollection services)
+            where TType : class
+        {
+            return services.RegisterAssembly(typeof(TType).Assembly);
+        }
+
+        /// <summary>
+        /// 根据Assembly自动依赖注入
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="assemblies"></param>
+        /// <returns></returns>
+        public static IServiceCollection RegisterAssembly(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            Check.NotNull(assemblies, nameof(assemblies));
+
+            services.Scan((scan) =>
+            {
+
+                scan.FromAssemblies(assemblies)
+                    // 瞬时
+                    .AddClasses((classes) =>
+                    {
+                        classes.AssignableTo<ITransientDependency>();
+                    })
+                    .AsSelf()
+                    .AsMatchingInterface()
+                    .WithTransientLifetime()
+
+                     // 单例
+                     .AddClasses((classes) =>
+                     {
+                         classes.AssignableTo<ISingletonDependency>();
+                     })
+                     .AsSelf()
+                     .AsMatchingInterface()
+                     .WithSingletonLifetime()
+
+                      // 范围
+                      .AddClasses((classes) =>
+                      {
+                          classes.AssignableTo<IScopeDependency>();
+                      })
+                     .AsSelf()
+                     .AsMatchingInterface()
+                     .WithScopedLifetime();
+            });
+
+            return services;
+        }
+    }
+}
