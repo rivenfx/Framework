b2e3440d04874d0a0f17b71540cd7f5c25b862fc
staneee <yi.hang@live.com>
2020-03-28 10:46:50 +0800

    增加Clock

---
+++ src/Riven/Timing/ClockProviders.cs
@@ -0,0 +1,11 @@
+﻿namespace Riven.Timing
+{
+    public static class ClockProviders
+    {
+        public static UnspecifiedClockProvider Unspecified { get; } = new UnspecifiedClockProvider();
+
+        public static LocalClockProvider Local { get; } = new LocalClockProvider();
+
+        public static UtcClockProvider Utc { get; } = new UtcClockProvider();
+    }
+}
---
+++ src/Riven/Timing/UnspecifiedClockProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Riven.Timing
+{
+    public class UnspecifiedClockProvider : IClockProvider
+    {
+        public DateTime Now => DateTime.Now;
+
+        public DateTimeKind Kind => DateTimeKind.Unspecified;
+
+        public bool SupportsMultipleTimezone => false;
+
+        public DateTime Normalize(DateTime dateTime)
+        {
+            return dateTime;
+        }
+
+        internal UnspecifiedClockProvider()
+        {
+
+        }
+    }
+}
