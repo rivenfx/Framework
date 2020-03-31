dc64243e5859a76458c50b14aa590f2449823c04
staneee <yi.hang@live.com>
2020-03-31 23:00:31 +0800

    权限校验demo

--- src/Riven.AspNetCore.Identity/Identity/Permissions/AppPermissions.cs
+++ src/Riven.AspNetCore.Identity/Identity/Permissions/AppPermissions.cs
@@ -1,14 +1,72 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Routing;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Riven.Identity.Permissions
 {
+
+    public class PermissionAttribute : Attribute
+    {
+
+    }
+
+    public class RoleClaimAuthorizationRequirement : IAuthorizationRequirement
+    {
+
+    }
+
+    public class RoleClaimAuthorizationHandler : AuthorizationHandler<RoleClaimAuthorizationRequirement>
+    {
+        public RoleClaimAuthorizationHandler()
+        {
+
+        }
+
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, RoleClaimAuthorizationRequirement requirement)
+        {
+            if (context.HasSucceeded || context.HasFailed)
+            {
+                return;
+            }
+
+            var routeEndpoint = context.Resource as RouteEndpoint;
+            var permissionAttr = routeEndpoint.Metadata.GetMetadata<PermissionAttribute>();
+
+            if (permissionAttr == null)
+            {
+                return;
+            }
+
+            var roles = context.User.FindAll(o => o.Type == ClaimTypes.Role).Select(o => o.Value).ToArray();
+            if (roles.Length == 0)
+            {
+                context.Fail();
+                return;
+            }
+
+
+
+
+            throw new NotImplementedException();
+        }
+    }
+
+
     /// <summary>
     /// 权限
     /// </summary>
     public class AppPermissions
     {
+        public AppPermissions()
+        {
+
+        }
+
         /// <summary>
         /// 父级权限名称
         /// </summary>
