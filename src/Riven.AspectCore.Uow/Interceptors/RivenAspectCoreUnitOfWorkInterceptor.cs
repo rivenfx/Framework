e920381183867517aede6b6606968c9d09102b90
staneee <yi.hang@live.com>
2020-04-12 21:34:37 +0800

    增加AspectCore Uow实现

---
+++ src/Riven.AspectCore.Uow/Interceptors/RivenAspectCoreUnitOfWorkInterceptor.cs
@@ -0,0 +1,49 @@
+﻿using AspectCore.DynamicProxy;
+using Riven.Uow;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Riven.Uow.Extensions;
+
+namespace Riven.Interceptors
+{
+    public class RivenAspectCoreUnitOfWorkInterceptor : AbstractInterceptor
+    {
+        public override async Task Invoke(AspectContext context, AspectDelegate next)
+        {
+            var unitOfWorkManager = context.ServiceProvider.GetService<IUnitOfWorkManager>();
+            var unitOfWorkAttr = context.ProxyMethod.GetUnitOfWorkAttributeOrNull();
+
+            if (unitOfWorkAttr == null)
+            {
+                // Default UnitOfWorkAttribute
+                unitOfWorkAttr = context.ServiceProvider.GetService<UnitOfWorkAttribute>();
+            }
+
+            if (unitOfWorkAttr.IsDisabled)
+            {
+                await next(context);
+                return;
+            }
+
+
+            var unitOfWorkOptions = unitOfWorkAttr.CreateOptions();
+
+            using (var uow = unitOfWorkManager.Begin(unitOfWorkOptions))
+            {
+                try
+                {
+                    await next(context);
+                    await uow.CompleteAsync(context.GetHttpContext().RequestAborted);
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+        }
+    }
+}
---
+++ src/Riven.AspectCore.Uow/RivenAspectCoreExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AspectCore.Configuration;
+using Riven.Interceptors;
+using Riven.Uow.Extensions;
+
+namespace Riven
+{
+    public static class RivenAspectCoreExtensions
+    {
+        /// <summary>
+        /// 添加 Riven UnitOfWork 拦截器
+        /// </summary>
+        /// <param name="aspectConfiguration"></param>
+        /// <returns></returns>
+        public static IAspectConfiguration AddRivenUnitOfWorkInterceptor(
+            [JetBrains.Annotations.NotNull]this IAspectConfiguration aspectConfiguration
+            )
+        {
+            Check.NotNull(aspectConfiguration, nameof(aspectConfiguration));
+
+            aspectConfiguration.Interceptors.AddTyped<RivenAspectCoreUnitOfWorkInterceptor>((method) =>
+            {
+                return method.GetUnitOfWorkAttributeOrNull() != null;
+            });
+
+            return aspectConfiguration;
+        }
+    }
+}
