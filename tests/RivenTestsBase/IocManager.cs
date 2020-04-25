bb82b56f3ca6904c453cc4a50e7231a862f6ed9b
staneee <yi.hang@live.com>
2020-04-25 12:23:42 +0800

    增加测试项目

---
+++ tests/RivenTestsBase/IocManager.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System
+{
+    public static class IocManager
+    {
+        public static IServiceCollection Services { get; private set; }
+
+        public static IServiceProvider Instance { get; private set; }
+
+        public static IConfiguration Configuration { get; private set; }
+
+
+        static IocManager()
+        {
+            Services = new ServiceCollection();
+
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true)
+                .Build();
+        }
+
+
+        public static void Build()
+        {
+            if (Instance != null)
+            {
+                return;
+            }
+
+            Instance = Services.BuildServiceProvider();
+        }
+    }
+}
