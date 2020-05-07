0ddce4369756a15eed1cc0d464bc3cafe29343bd
staneee <yi.hang@live.com>
2020-05-07 22:05:51 +0800

    Mapster增加注册扩展函数

---
+++ src/Riven.Mapster/RivenMapsterExtensions.cs
@@ -0,0 +1,41 @@
+﻿using JetBrains.Annotations;
+using Mapster;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using System.Text;
+
+namespace Riven
+{
+    public static class RivenMapsterExtensions
+    {
+        /// <summary>
+        /// 全局映射配置 - 扫描程序集注册的对象映射
+        /// </summary>
+        /// <param name="assembly"></param>
+        public static void RegisterGlobalObjectMapper([NotNull]this Assembly assembly)
+        {
+            Check.NotNull(assembly, nameof(assembly));
+
+            TypeAdapterConfig.GlobalSettings.Scan(assembly);
+        }
+
+        /// <summary>
+        /// 指定映射配置 - 扫描程序集注册的对象映射
+        /// </summary>
+        /// <param name="typeAdapterConfig"></param>
+        /// <param name="assemblies"></param>
+        public static void RegisterObjectMapper([NotNull]this TypeAdapterConfig typeAdapterConfig, params Assembly[] assemblies)
+        {
+            Check.NotNull(typeAdapterConfig, nameof(typeAdapterConfig));
+
+            if (assemblies == null || assemblies.Length == 0)
+            {
+                return;
+            }
+
+            typeAdapterConfig.Scan(assemblies);
+        }
+    }
+}
