2f1bad85173bb116b0e54f38485e694c633b9ffa
staneee <yi.hang@live.com>
2020-07-20 22:46:20 +0800

    增加linq扩展和反射帮助函数

---
+++ src/Riven/Linq/RivenLinqExtenstions.cs
@@ -0,0 +1,24 @@
+﻿using Riven.Linq.Expressions;
+
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace System.Linq
+{
+   public static class RivenLinqExtenstions
+    {
+        /// <summary>
+        /// 合并表达式
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression1"></param>
+        /// <param name="expression2"></param>
+        /// <returns></returns>
+        public static Expression<Func<T, bool>> CombineExpressions<T>(this Expression<Func<T, bool>> expression1, Expression<Func<T, bool>> expression2)
+        {
+            return ExpressionCombiner.Combine(expression1, expression2);
+        }
+    }
+}
