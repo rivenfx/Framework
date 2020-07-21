2dd9473079ec4ea5f92edb9ad49a457e32aed0ab
staneee <yi.hang@live.com>
2020-07-21 22:08:56 +0800

    增加全局过滤器扩展函数

--- src/Riven.Domain.EntityFrameworkCore/DbContextHelper.cs
+++ src/Riven.Domain.EntityFrameworkCore/DbContextHelper.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using JetBrains.Annotations;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
@@ -9,8 +13,35 @@ namespace Riven
     {
         public static string HardDelete { get; } = "HardDelete";
 
-        public static MethodInfo ConfigureGlobalFiltersMethodInfo = typeof(IRivenFilterDbContext).GetMethod(nameof(IRivenFilterDbContext.ConfigureGlobalFilters), BindingFlags.Instance | BindingFlags.Public);
+        public static MethodInfo ConfigureGlobalFiltersMethodInfo = typeof(IRivenDbContext).GetMethod(nameof(IRivenDbContext.ConfigureGlobalFilters), BindingFlags.Instance | BindingFlags.Public);
 
+        /// <summary>
+        /// 配置查询全局过滤器
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        /// <param name="dbContext">数据库上下文</param>
+        /// <returns></returns>
+        public static ModelBuilder ConfigureGlobalFilters([NotNull] this ModelBuilder modelBuilder, [NotNull] DbContext dbContext)
+        {
+            Check.NotNull(modelBuilder, nameof(modelBuilder));
+            Check.NotNull(dbContext, nameof(dbContext));
 
+            if (dbContext is IRivenDbContext rivenDbContext)
+            {
+                if (rivenDbContext.ServiceProvider == null)
+                {
+                    return modelBuilder;
+                }
+
+                foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    DbContextHelper.ConfigureGlobalFiltersMethodInfo
+                        .MakeGenericMethod(entityType.ClrType)
+                        .Invoke(rivenDbContext, new object[] { modelBuilder, entityType });
+                }
+            }
+
+            return modelBuilder;
+        }
     }
 }
