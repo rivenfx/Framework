c45934e6d4597ac91631717673ebc98515aaaaec
staneee <yi.hang@live.com>
2020-08-09 11:15:33 +0800

    增加DbProvider Storage

---
+++ src/Riven.UnitOfWork.EntityFrameworkCore/IDbContextProviderStorage.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using Riven.Storage;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Riven
+{
+    /// <summary>
+    /// DbContext Provider Storage
+    /// </summary>
+    public interface IDbContextProviderStorage : IAnyStorage<IDbContextProvider>
+    {
+    }
+
+    public class DefaultDbContextProviderStorage : AnyStorageBase<IDbContextProvider>, IDbContextProviderStorage
+    {
+        public DefaultDbContextProviderStorage(IServiceProvider serviceProvider)
+        {
+            var dbContextProviders = serviceProvider.GetServices<IDbContextProvider>()
+                .ToDictionary(o => o.Name);
+            foreach (var item in dbContextProviders)
+            {
+                this.AddOrUpdate(item.Key, item.Value);
+            }
+        }
+    }
+}
