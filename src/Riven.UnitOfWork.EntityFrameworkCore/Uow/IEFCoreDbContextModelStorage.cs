be836884c4a00b4aed2ffcd2372ed05d953a5a13
staneee <yi.hang@live.com>
2020-08-09 11:15:55 +0800

    增加DbConext Model Storage

---
+++ src/Riven.UnitOfWork.EntityFrameworkCore/Uow/IEFCoreDbContextModelStorage.cs
@@ -0,0 +1,31 @@
+﻿using JetBrains.Annotations;
+
+using Microsoft.EntityFrameworkCore.Metadata;
+
+using Riven.Storage;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riven.Uow
+{
+    /// <summary>
+    /// DbContext Model Storage
+    /// </summary>
+    public interface IEFCoreDbContextModelStorage : IAnyStorage<IModel>
+    {
+
+    }
+
+    public class DefaultEFCoreDbContextModelStorage : AnyStorageBase<IModel>, IEFCoreDbContextModelStorage
+    {
+        public override void AddOrUpdate([NotNull] string key, [NotNull] IModel val)
+        {
+            Check.NotNull(key, nameof(key));
+            Check.NotNull(val, nameof(val));
+
+            _dataMap.TryAdd(key, val);
+        }
+    }
+}
