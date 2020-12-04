75a0dbf81c5eb92612fcdaec4dc11afe260e7088
staneee <yi.hang@live.com>
2020-12-04 16:50:58 +0800

    增加多租户配置项类型

---
+++ src/Riven.Domain/MultiTenancy/IMultiTenancyOptions.cs
@@ -0,0 +1,33 @@
+namespace Riven.MultiTenancy
+{
+    /// <summary>
+    /// 多租户配置项
+    /// </summary>
+    public interface IMultiTenancyOptions
+    {
+        /// <summary>
+        /// 是否启用多租户
+        /// </summary>
+        bool IsEnabled { get; }
+
+        /// <summary>
+        /// 租户名称键值
+        /// </summary>
+        string TenantNameKey { get; }
+    }
+
+    public class DefaultMultiTenancyOptions : IMultiTenancyOptions
+    {
+        public virtual bool IsEnabled
+        {
+            get => MultiTenancyConfig.IsEnabled;
+            set => MultiTenancyConfig.IsEnabled = value;
+        }
+
+        public virtual string TenantNameKey
+        {
+            get => MultiTenancyConfig.TenantNameKey;
+            set => MultiTenancyConfig.TenantNameKey = value;
+        }
+    }
+}
