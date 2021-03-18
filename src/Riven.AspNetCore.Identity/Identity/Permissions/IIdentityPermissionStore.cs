1f88c826641228ea78ab0e5aa0b4e8f73145eeda
staneee <yi.hang@live.com>
2021-03-18 15:11:34 +0800

    修改权限查询器

--- src/Riven.AspNetCore.Identity/Identity/Permissions/IIdentityPermissionStore.cs
+++ src/Riven.AspNetCore.Identity/Identity/Permissions/IIdentityPermissionStore.cs
@@ -3,19 +3,24 @@ using JetBrains.Annotations;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Riven.Identity.Permissions
 {
-    public interface IIdentityPermissionStore<TPermission>
+    public interface IIdentityPermissionStore<TPermission> : IIdentityPermissionFinder, IDisposable
         where TPermission : IdentityPermission
     {
+        /// <summary>
+        /// 查询器
+        /// </summary>
+        IQueryable<TPermission> Query { get; }
+
         /// <summary>
         /// 获取所有权限
         /// </summary>
         /// <returns></returns>
         Task<IEnumerable<string>> GetAll();
-
 
         /// <summary>
         /// 创建权限
@@ -23,18 +28,6 @@ namespace Riven.Identity.Permissions
         /// <param name="permission"></param>
         /// <returns></returns>
         Task CreateAsync([NotNull] TPermission permission);
-
-
-        /// <summary>
-        /// 查找权限
-        /// </summary>
-        /// <param name="type">权限类型.
-        /// <see cref="IdentityPermission{TKey}.Type"/>
-        /// </param>
-        /// <param name="provider">类型映射关联的数据.</param>
-        /// <returns></returns>
-        Task<IEnumerable<string>> FindPermissions(string type, string provider);
-
 
         /// <summary>
         /// 删除权限
--- src/Riven.AspNetCore.Identity/Identity/Permissions/IdentityPermissionStore.cs
+++ src/Riven.AspNetCore.Identity/Identity/Permissions/IdentityPermissionStore.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Riven.Identity.Permissions
@@ -13,31 +14,27 @@ namespace Riven.Identity.Permissions
     public class IdentityPermissionStore<TPermission> : IIdentityPermissionStore<TPermission>
         where TPermission : IdentityPermission
     {
+        protected static List<string> _emptyPermissionNames = new List<string>();
+
+        protected bool _disposed;
+
+        public bool AutoSaveChanges { get; set; } = true;
+
         public virtual DbContext Context => throw new NotImplementedException(nameof(Context));
 
-        public virtual IQueryable<TPermission> Permissions => Context.Set<TPermission>();
+        public virtual IQueryable<TPermission> Query => Context.Set<TPermission>();
 
-        public async Task<IEnumerable<string>> GetAll()
-        {
-            return await Permissions.AsNoTracking()
-                   .GroupBy(o => o.Name)
-                   .Select(o => o.Key)
-                   .ToListAsync();
-        }
+
 
         public async Task CreateAsync([NotNull] TPermission permission)
         {
             Check.NotNull(permission, nameof(permission));
 
-            await Context.AddAsync(permission);
-        }
+            ThrowIfDisposed();
 
-        public async Task<IEnumerable<string>> FindPermissions(string type, string provider)
-        {
-            return await Permissions.AsNoTracking()
-                    .Where(o => o.Type == type && o.Provider == provider)
-                    .Select(o => o.Name)
-                    .ToListAsync();
+            await Context.AddAsync(permission);
+
+            await this.SaveChanges(default);
         }
 
         public async Task Remove(params string[] names)
@@ -47,103 +44,101 @@ namespace Riven.Identity.Permissions
                 return;
             }
 
+            ThrowIfDisposed();
+
             await Task.Yield();
 
 
-            var permissions = await Permissions
+            var permissions = await Query
                 .Where(o => names.Contains(o.Name))
                 .ToListAsync();
 
             this.Context.RemoveRange(permissions);
+
+            await this.SaveChanges(default);
+        }
+
+
+        public async Task<IEnumerable<string>> GetAll()
+        {
+            return await Query.AsNoTracking()
+                   .GroupBy(o => o.Name)
+                   .Select(o => o.Key)
+                   .ToListAsync();
+        }
+
+
+        public async Task<IEnumerable<string>> FindPermissions(string type, string provider)
+        {
+            if (string.IsNullOrWhiteSpace(type)
+                || string.IsNullOrWhiteSpace(provider)
+                )
+            {
+                return _emptyPermissionNames;
+            }
+
+            return await Query.AsNoTracking()
+                    .Where(o => o.Type == type && o.Provider == provider)
+                    .Select(o => o.Name)
+                    .ToListAsync();
         }
 
 
 
-        ///// <summary>
-        ///// 校验权限
-        ///// </summary>
-        ///// <param name="name">
-        ///// <see cref="TPermission.Name"/>
-        ///// </param>
-        ///// <param name="type">
-        ///// <see cref="TPermission.Type"/>
-        ///// </param>
-        ///// <param name="provider">
-        ///// <see cref="TPermission.Provider"/>
-        ///// </param>
-        ///// <returns></returns>
-        //public async Task<bool> IsGrantedAsync(string name, string type, string provider)
-        //{
-        //    if (string.IsNullOrWhiteSpace(name))
-        //    {
-        //        return true;
-        //    }
+        public async Task<bool> ExistPermission(string name, string type, string provider)
+        {
+            if (string.IsNullOrWhiteSpace(name)
+                || string.IsNullOrWhiteSpace(type)
+                || string.IsNullOrWhiteSpace(provider)
+                )
+            {
+                return false;
+            }
+
+            ThrowIfDisposed();
+
+            return await Query.AsNoTracking()
+                   .Where(o => o.Name == name && o.Type == type && o.Provider == provider)
+                   .AnyAsync();
+        }
+
+        public async Task<IEnumerable<string>> FindPermissions(string type, string[] providers)
+        {
+            if (providers == null || providers.Length == 0 || string.IsNullOrWhiteSpace(type))
+            {
+                return _emptyPermissionNames;
+            }
+
+            return await Query.AsNoTracking()
+                    .Where(o => o.Type == type && providers.Contains(o.Provider))
+                    .Select(o => o.Name)
+                    .ToListAsync();
+        }
 
 
-        //    var count = await Permissions.CountAsync(o => o.Name == name
-        //            && o.Type == type
-        //            && o.Provider == provider);
 
-        //    return count > 0;
-        //}
+        protected virtual Task SaveChanges(CancellationToken cancellationToken)
+        {
+            return AutoSaveChanges ? Context.SaveChangesAsync(cancellationToken) : Task.CompletedTask;
+        }
 
-        ///// <summary>
-        ///// 多个校验权限
-        ///// </summary>
-        ///// <param name="names">
-        ///// <see cref="TPermission.Name"/>
-        ///// </param>
-        ///// <param name="type">
-        ///// <see cref="TPermission.Type"/>
-        ///// </param>
-        ///// <param name="provider">
-        ///// <see cref="TPermission.Provider"/>
-        ///// </param>
-        ///// <param name="requireAll">是否要校验所有权限</param>
-        ///// <returns></returns>
-        //public async Task<MultiPermissionGrantResult> IsGrantedAsync(string[] names, string type, string provider, bool requireAll = false)
-        //{
-        //    var result = new MultiPermissionGrantResult();
+        /// <summary>
+        /// Throws if this class has been disposed.
+        /// </summary>
+        protected void ThrowIfDisposed()
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException(GetType().Name);
+            }
+        }
 
-        //    if (names == null || names.Length == 0)
-        //    {
-        //        return MultiPermissionGrantResult.SuccessResult;
-        //    }
-
-        //    var successed = false;
-        //    // 校验所有权限
-        //    if (requireAll)
-        //    {
-
-        //        foreach (var item in names)
-        //        {
-        //            successed = await this.IsGrantedAsync(item, type, provider);
-
-        //            if (!successed)
-        //            {
-        //                result.Unsuccessful.Add(item);
-        //            }
-        //        }
-
-        //        result.SetSuccessed(!result.HasError);
-
-        //        return result;
-        //    }
-
-        //    // 只校验部分权限
-        //    foreach (var item in names)
-        //    {
-        //        successed = await this.IsGrantedAsync(item, type, provider);
-        //        if (successed)
-        //        {
-        //            result.SetSuccessed(true);
-        //            break;
-        //        }
-        //    }
-
-        //    return result;
-        //}
-
-
+        /// <summary>
+        /// Dispose the stores
+        /// </summary>
+        public void Dispose()
+        {
+            _disposed = true;
+        }
     }
 }
