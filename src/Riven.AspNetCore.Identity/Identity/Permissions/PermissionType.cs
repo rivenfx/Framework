dac0465f13673f228fde446fbad744559e0b3c5f
staneee <yi.hang@live.com>
2021-03-18 09:39:20 +0800

    默认的权限类型

---
+++ src/Riven.AspNetCore.Identity/Identity/Permissions/PermissionType.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riven.Identity.Permissions
+{
+    /// <summary>
+    /// 权限类型
+    /// </summary>
+    public static class PermissionType
+    {
+        /// <summary>
+        /// 角色
+        /// </summary>
+        public const string Role = "R";
+
+        /// <summary>
+        /// 用户
+        /// </summary>
+        public const string User = "U";
+    }
+}
