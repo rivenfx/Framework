331842819939db5476da4dac99555e41a62d0fc8
staneee <yi.hang@live.com>
2021-06-28 17:05:01 +0800

    增加uowhelper

---
+++ src/Riven.UnitOfWork/Uow/UnitOfWorkHelper.cs
@@ -0,0 +1,33 @@
+using Riven.Uow.Extensions;
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Riven.Uow
+{
+    /// <summary>
+    /// 工作单元帮助类
+    /// </summary>
+    public static class UnitOfWorkHelper
+    {
+        /// <summary>
+        /// 是否为工作单元方法
+        /// </summary>
+        /// <param name="methodInfo">方法信息</param>
+        /// <param name="unitOfWorkAttribute">工作单元参数</param>
+        /// <returns></returns>
+        public static bool IsUnitOfWorkMethod(MethodInfo methodInfo, out UnitOfWorkAttribute unitOfWorkAttribute)
+        {
+            unitOfWorkAttribute = methodInfo.GetUnitOfWorkAttributeOrNull();
+
+            if (unitOfWorkAttribute == null || unitOfWorkAttribute.IsDisabled)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
