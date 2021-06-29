8ca4c11cc8c8775f42731f028d27371d4eca2644
staneee <yi.hang@live.com>
2021-06-29 20:26:32 +0800

    增加静态文件扩展函数

---
+++ src/Riven.AspNetCore/AspNetCore/Builder/RivenStaticFileExtensions.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Builder;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class RivenStaticFileExtensions
+    {
+        /// <summary>
+        ///  启用默认的静态文件服务，并将默认静态文件映射到 baseHref 的 request path
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="baseHref">baseHref,必须以“/”开头，若值为 “null”、“/”、空字符 都不会生效</param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseStaticFilesWithBaseHref(this IApplicationBuilder app,string baseHref)
+        {
+            // 默认的静态文件
+            // 默认的静态文件
+            app.UseStaticFiles();
+
+            // basehref 不等于 / 时需要映射 wwwroot 到 basehref 地址
+            if (!string.IsNullOrWhiteSpace(baseHref)
+                && baseHref.Trim() != "/")
+            {
+                app.UseStaticFiles(baseHref.Trim());
+            }
+
+            return app;
+        }
+    }
+}
